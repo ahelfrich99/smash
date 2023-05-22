46502f68dcfa6cb4bcaa5d8a3be8a9a2440748fe
Adrianna Helfrich <adriannahelfrich99@gmail.com>
2023-05-22 23:07:19 +0000

    BO-10

---
+++ bangerz/migrations/001_migration.py
@@ -0,0 +1,140 @@
+steps = [
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE bangerz (
+            id SERIAL PRIMARY KEY NOT NULL,
+            user_id SERIAL NOT NULL,
+            song_title VARCHAR(500) NOT NULL,
+            artist VARCHAR(500) NOT NULL,
+            album VARCHAR(500),
+            song_img VARCHAR(500),
+            date DATE NOT NULL
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE bangerz;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE comments (
+            id SERIAL PRIMARY KEY NOT NULL,
+            user_id SERIAL NOT NULL REFERENCES USERS(ID),
+            post_id SERIAL NOT NULL REFERENCES POSTS(ID),
+            content TEXT NOT NULL,
+            date DATE NOT NULL,
+            like_count INT DEFAULT 0
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE comments;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE group_comments (
+            id SERIAL PRIMARY KEY NOT NULL,
+            group_id SERIAL NOT NULL REFERENCES GROUPS(ID),
+            user_id SERIAL NOT NULL REFERENCES USERS(ID)
+            post_id SERIAL NOT NULL REFERENCES POSTS(ID),
+            content TEXT NOT NULL,
+            date DATE NOT NULL,
+            like_count INT DEFAULT 0
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE group_comments;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE group_posts (
+            id SERIAL PRIMARY KEY NOT NULL,
+            group_id SERIAL NOT NULL,
+            user_id SERIAL NOT NULL REFERENCES USERS(ID),
+            banger_id SERIAL REFERENCES BANGERZ(ID),
+            content TEXT NOT NULL,
+            date DATE NOT NULL,
+            like_count INT DEFAULT 0
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE group_posts;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE groups (
+            id SERIAL PRIMARY KEY NOT NULL,
+            group_name VARCHAR(50) NOT NULL,
+            group_size INT NOT NULL,
+            picture_url VARCHAR(500) NOT NULL,
+            description TEXT NOT NULL
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE groups;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE homies (
+            id SERIAL PRIMARY KEY NOT NULL,
+            user_id SERIAL NOT NULL REFERENCES USERS(ID),
+            online BOOLEAN NOT NULL
+
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE homies;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE posts (
+            id SERIAL PRIMARY KEY NOT NULL,
+            user_id SERIAL NOT NULL REFERENCES USERS(ID),
+            banger_id SERIAL REFERENCES BANGERZ(ID),
+            text TEXT NOT NULL,
+            like_count INT DEFAULT 0
+
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE posts;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY NOT NULL,
+            username VARCHAR(15) NOT NULL,
+            password VARCHAR(20) NOT NULL,
+            first_name VARCHAR(50) NOT NULL,
+            last_name VARCHAR(50) NOT NULL,
+            email VARCHAR(50) NOT NULL,
+            profile_img VARCHAR(500) NOT NULL
+
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE users;
+        """,
+    ],
+]
